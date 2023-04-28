70606d34ddcc5493430db36955871c5644275951
Nick Marshall <nmarshall@highkeep.com>
2023-04-27 18:02:28 -0700

    testing gpg --recv-keys option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 FILE="$(basename "$0")"
 
 # Enable the multilib repository
-cat << EOM >> /etc/pacman.conf
+cat <<EOM >>/etc/pacman.conf
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 EOM
 
 # Add alerque repository for paru
-cat << EOM >> /etc/pacman.conf
+cat <<EOM >>/etc/pacman.conf
 [alerque]
 SigLevel = Optional TrustAll
 Server = https://arch.alerque.com/\$arch
@@ -37,6 +37,15 @@ if [ "${INPUT_NVIDIAUTILS:-false}" == true ]; then
     pacman -Syu --noconfirm --needed multilib-devel nvidia-utils lib32-nvidia-utils
 fi
 
+if [ -n "${INPUT_RECVKEYS:-}" ]; then
+    readarray -td '' publicKeys < <(awk '{ gsub(/, /,"\0"); print; }' <<<"${INPUT_RECVKEYS:-}, ")
+    unset 'publicKeys[-1]'
+
+    for key in ${publicKeys[@]}; do
+        gpg --recv-keys ${key}
+    done
+fi
+
 # Makepkg does not allow running as root
 # Create a new user `builder`
 # `builder` needs to have a home directory because some PKGBUILDs will try to
@@ -44,7 +53,7 @@ fi
 useradd builder -m
 # When installing dependencies, makepkg will use sudo
 # Give user `builder` passwordless sudo access
-echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "builder ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 
 # Give all users (particularly builder) full access to these files
 chmod -R a+rw .
@@ -52,7 +61,7 @@ chmod -R a+rw .
 BASEDIR="$PWD"
 cd "${INPUT_PKGDIR:-.}"
 
-function download_database () {
+function download_database() {
     # Download the repository files if a repository tag has been specified
     # This is put here to fail early in case they weren't downloaded
     REPOFILES=("${INPUT_REPORELEASETAG:-}".{db{,.tar.gz},files{,.tar.gz}})
@@ -76,19 +85,19 @@ fi
 # Assume that if .SRCINFO is missing then it is generated elsewhere.
 # AUR checks that .SRCINFO exists so a missing file can't go unnoticed.
 if [ -f .SRCINFO ] && ! sudo -u builder makepkg --printsrcinfo | diff - .SRCINFO; then
-	echo "::error file=$FILE,line=$LINENO::Mismatched .SRCINFO. Update with: makepkg --printsrcinfo > .SRCINFO"
-	exit 1
+    echo "::error file=$FILE,line=$LINENO::Mismatched .SRCINFO. Update with: makepkg --printsrcinfo > .SRCINFO"
+    exit 1
 fi
 
 # Optionally install dependencies from AUR
 if [ -n "${INPUT_AURDEPS:-}" ]; then
-	# First install paru
-	pacman -Syu --noconfirm paru
+    # First install paru
+    pacman -Syu --noconfirm paru
 
-	# Extract dependencies from .SRCINFO (depends or depends_x86_64) and install
-	mapfile -t PKGDEPS < \
-		<(sed -n -e 's/^[[:space:]]*\(make\)\?depends\(_x86_64\)\? = \([[:alnum:][:punct:]]*\)[[:space:]]*$/\3/p' .SRCINFO)
-	sudo -H -u builder paru --sync --noconfirm "${PKGDEPS[@]}"
+    # Extract dependencies from .SRCINFO (depends or depends_x86_64) and install
+    mapfile -t PKGDEPS < \
+        <(sed -n -e 's/^[[:space:]]*\(make\)\?depends\(_x86_64\)\? = \([[:alnum:][:punct:]]*\)[[:space:]]*$/\3/p' .SRCINFO)
+    sudo -H -u builder paru --sync --noconfirm "${PKGDEPS[@]}"
 fi
 
 # Make the builder user the owner of these files
@@ -104,7 +113,7 @@ sudo -H -u builder makepkg --syncdeps --noconfirm ${INPUT_MAKEPKGARGS:-}
 
 # Get array of packages to be built
 # shellcheck disable=SC2086
-mapfile -t PKGFILES < <( sudo -u builder makepkg --packagelist ${INPUT_MAKEPKGARGS:-})
+mapfile -t PKGFILES < <(sudo -u builder makepkg --packagelist ${INPUT_MAKEPKGARGS:-})
 echo "Package(s): ${PKGFILES[*]}"
 
 if [ -n "${INPUT_REPORELEASETAG:-}" ]; then
@@ -124,21 +133,21 @@ for PKGFILE in "${PKGFILES[@]}"; do
         mv "$PKGFILE" "$dest"
         PKGFILE="$dest"
     fi
-	# makepkg reports absolute paths, must be relative for use by other actions
-	RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
-	# Caller arguments to makepkg may mean the pacakge is not built
-	if [ -f "$PKGFILE" ]; then
-		echo "pkgfile$i=$RELPKGFILE" >> $GITHUB_OUTPUT
+    # makepkg reports absolute paths, must be relative for use by other actions
+    RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
+    # Caller arguments to makepkg may mean the pacakge is not built
+    if [ -f "$PKGFILE" ]; then
+        echo "pkgfile$i=$RELPKGFILE" >>$GITHUB_OUTPUT
         # Optionally add the packages to a makeshift repository in GitHub releases
         if [ -n "${INPUT_REPORELEASETAG:-}" ]; then
             sudo -u builder repo-add "${INPUT_REPORELEASETAG:-}".db.tar.gz "$(basename "$PKGFILE")"
         else
             echo "Skipping repository update for $RELPKGFILE"
         fi
-	else
-		echo "Archive $RELPKGFILE not built"
-	fi
-	(( ++i ))
+    else
+        echo "Archive $RELPKGFILE not built"
+    fi
+    ((++i))
 done
 
 if [ -n "${INPUT_REPORELEASETAG:-}" ]; then
@@ -151,52 +160,52 @@ if [ -n "${INPUT_REPORELEASETAG:-}" ]; then
     j=0
     for REPOFILE in "${REPOFILES[@]}"; do
         RELREPOFILE="$(realpath --relative-base="$BASEDIR" "$(realpath -s "$REPOFILE")")"
-        echo "repofile$j=$RELREPOFILE" >> $GITHUB_OUTPUT
-    (( ++j ))
+        echo "repofile$j=$RELREPOFILE" >>$GITHUB_OUTPUT
+        ((++j))
     done
 fi
 
-function prepend () {
-	# Prepend the argument to each input line
-	while read -r line; do
-		echo "$1$line"
-	done
+function prepend() {
+    # Prepend the argument to each input line
+    while read -r line; do
+        echo "$1$line"
+    done
 }
 
 function namcap_check() {
-	# Run namcap checks
-	# Installing namcap after building so that makepkg happens on a minimal
-	# install where any missing dependencies can be caught.
-	pacman -S --noconfirm --needed namcap
+    # Run namcap checks
+    # Installing namcap after building so that makepkg happens on a minimal
+    # install where any missing dependencies can be caught.
+    pacman -S --noconfirm --needed namcap
 
-	NAMCAP_ARGS=()
-	if [ -n "${INPUT_NAMCAPRULES:-}" ]; then
-		NAMCAP_ARGS+=( "-r" "${INPUT_NAMCAPRULES}" )
-	fi
-	if [ -n "${INPUT_NAMCAPEXCLUDERULES:-}" ]; then
-		NAMCAP_ARGS+=( "-e" "${INPUT_NAMCAPEXCLUDERULES}" )
-	fi
+    NAMCAP_ARGS=()
+    if [ -n "${INPUT_NAMCAPRULES:-}" ]; then
+        NAMCAP_ARGS+=("-r" "${INPUT_NAMCAPRULES}")
+    fi
+    if [ -n "${INPUT_NAMCAPEXCLUDERULES:-}" ]; then
+        NAMCAP_ARGS+=("-e" "${INPUT_NAMCAPEXCLUDERULES}")
+    fi
 
-	# For reasons that I don't understand, sudo is not resetting '$PATH'
-	# As a result, namcap finds program paths in /usr/sbin instead of /usr/bin
-	# which makes namcap fail to identify the packages that provide the
-	# program and so it emits spurious warnings.
-	# More details: https://bugs.archlinux.org/task/66430
-	#
-	# Work around this issue by putting bin ahead of sbin in $PATH
-	export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+    # For reasons that I don't understand, sudo is not resetting '$PATH'
+    # As a result, namcap finds program paths in /usr/sbin instead of /usr/bin
+    # which makes namcap fail to identify the packages that provide the
+    # program and so it emits spurious warnings.
+    # More details: https://bugs.archlinux.org/task/66430
+    #
+    # Work around this issue by putting bin ahead of sbin in $PATH
+    export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 
-	namcap "${NAMCAP_ARGS[@]}" PKGBUILD \
-		| prepend "::warning file=$FILE,line=$LINENO::"
-	for PKGFILE in "${PKGFILES[@]}"; do
-		if [ -f "$PKGFILE" ]; then
-			RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
-			namcap "${NAMCAP_ARGS[@]}" "$PKGFILE" \
-				| prepend "::warning file=$FILE,line=$LINENO::$RELPKGFILE:"
-		fi
-	done
+    namcap "${NAMCAP_ARGS[@]}" PKGBUILD |
+        prepend "::warning file=$FILE,line=$LINENO::"
+    for PKGFILE in "${PKGFILES[@]}"; do
+        if [ -f "$PKGFILE" ]; then
+            RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
+            namcap "${NAMCAP_ARGS[@]}" "$PKGFILE" |
+                prepend "::warning file=$FILE,line=$LINENO::$RELPKGFILE:"
+        fi
+    done
 }
 
 if [ -z "${INPUT_NAMCAPDISABLE:-}" ]; then
-	namcap_check
+    namcap_check
 fi
